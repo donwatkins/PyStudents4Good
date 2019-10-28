6ff5e6ee1f6cd8625cfa135fdbc23292fd790832
Donald Watkins <don@dgwenterprises.biz>
2019-10-28 14:05:05 -0400

    Add files via upload

---
+++ PythonCodeExamples/TurtlePython/first_turtle.py
@@ -0,0 +1,10 @@
+# my first turtle program --- Provides simple comment for your code
+import turtle as t # immport the turtle module symbolized by the letter 't'
+t.forward(100) #this moves the turtle forward 100 turtle steps
+t.right(90) #this turns the turtle 90 degrees
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
