d400e60bfb19c2647b157dc0bcc8ef11fb93aa21
Donald Watkins <don@dgwenterprises.biz>
2022-10-10 22:26:48 -0400

    Add files via upload
    Simple conversion from miles to kilometers.

---
+++ PythonCodeExamples/DistanceWalked.py
@@ -0,0 +1,13 @@
+# A program to convert from miles to kilometers
+print("What is your name?")
+myName = input()
+print("Hello " + myName) # Asks your name
+
+print("How many miles did you walk? ") #Aske for distance walked in miles
+myMiles = input()
+
+#prints the distance in kilometers
+
+print("The distance you walked in kilometers is..")
+kilometer = 1.60934
+print(kilometer * (float(myMiles)))
