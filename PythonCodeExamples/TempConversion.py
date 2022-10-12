1d4da17566088119112c9b1543407171a339822f
Donald Watkins <don@dgwenterprises.biz>
2022-10-11 23:43:49 -0400

    Add files via upload
    A short program I wrote to convert to celsius from fahrenheit using Python.

---
+++ PythonCodeExamples/TempConversion.py
@@ -0,0 +1,7 @@
+# Short program to convert from fahrenheit to celsius
+fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+celsius = (fahrenheit - 32) * 5/9
+
+#print the converted temperature
+print("The temperature in celsius is")
+print(celsius)
