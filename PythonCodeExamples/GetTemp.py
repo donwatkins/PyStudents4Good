4e010aaa166867e40fa87ac193ede7f2514a696e
Donald Watkins <don@dgwenterprises.biz>
2022-10-14 18:42:13 -0400

    Add files via upload
    Getting a temperature conversion with a function.

--- PythonCodeExamples/GetTemp.py
+++ PythonCodeExamples/GetTemp.py
@@ -1,7 +1,7 @@
 # Temperature conversion with a function
 
 def getTemp(temp):
-    print(float(temp) -32 * 5/9)
+    print((float(temp) -32 ) * 5/9)
 
 print("What is the current temperature? ")
 temp = input("What is the temperature?")
