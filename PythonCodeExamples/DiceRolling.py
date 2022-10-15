abea3e5e43bd0d6e9a8ac9caf4867dceb6725064
Donald Watkins <don@dgwenterprises.biz>
2022-10-14 23:39:44 -0400

    Add files via upload
    Another dice roll with Python

---
+++ PythonCodeExamples/DiceRolling.py
@@ -0,0 +1,9 @@
+# Dice Rolling program
+import random
+a = random.randint(1,6)
+b = random.randint(1,6)
+
+# result of the roll of each pair of dice
+print("The results of your dice roll are...")
+print(a)
+print(b)
